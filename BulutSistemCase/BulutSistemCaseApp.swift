7e678af14949d8f0a9c41920314b9f8f3ea84d8a
Berkay Tuncel <berkaytncl98@gmail.com>
2024-05-03 12:17:57 +0300

    Initial Commit

---
+++ BulutSistemCase/BulutSistemCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulutSistemCaseApp.swift
+//  BulutSistemCase
+//
+//  Created by Berkay Tuncel on 3.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct BulutSistemCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
